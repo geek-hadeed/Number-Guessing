f1564173a455ab971c931194e06689b27075270e
Hadeed Hussain <programmerhadeed@gmail.com>
2024-03-23 18:41:07 +0500

    Completed

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-// let GuessNumber = "18"
 let GuessNumber = Math.floor(Math.random() * 10 + 1);
 const guess = await inquirer.prompt([
-    { message: "Guess A Number 1-10 : ", Type: "number", name: "UserNumber" }
+    { message: "Guess A Number 1-10: ", type: "number", name: "UserNumber" }
 ]);
 if (guess.UserNumber === GuessNumber) {
     console.log("You Guessed Right");
--- main.ts
+++ main.ts
@@ -2,20 +2,18 @@
 
 import inquirer from "inquirer";
 
-// let GuessNumber = "18"
-
-let GuessNumber = Math.floor(Math.random() *10 +1)
+let GuessNumber: number = Math.floor(Math.random() * 10 + 1);
 
 const guess = await inquirer.prompt([
-    { message: "Guess A Number 1-10 : " ,Type: "number" ,name: "UserNumber"  }   
-    ])
+{ message: "Guess A Number 1-10: ", type: "number", name: "UserNumber" }
+]);
 
-if(guess.UserNumber === "GuessNumber"){
-    console.log("You Guessed Right")
+if (guess.UserNumber === GuessNumber) 
+{
+console.log("You Guessed Right");
+} 
+else
+{
+console.log("You Guessed Wrong");
+console.log(`The Number Was ${GuessNumber}`);
 }
-
-else{
-    console.log("You Guessed Wrong")
-    console.log(`The Number Was ${GuessNumber}`)
-}
-
