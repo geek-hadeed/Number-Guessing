3bafa7b807ab257232bd9159864d9229329b6e04
Hadeed Hussain <programmerhadeed@gmail.com>
2024-03-23 17:49:02 +0500

    first

---
+++ main.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+// let GuessNumber = "18"
+let GuessNumber = Math.floor(Math.random() * 10 + 1);
+const guess = await inquirer.prompt([
+    { message: "Guess A Number 1-10 : ", Type: "number", name: "UserNumber" }
+]);
+if (guess.UserNumber === GuessNumber) {
+    console.log("You Guessed Right");
+}
+else {
+    console.log("You Guessed Wrong");
+    console.log(`The Number Was ${GuessNumber}`);
+}
